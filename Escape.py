55f812cbc4bc9f9bd647a3d813cfb4ecf06cf8d6
SAMBIT PATRA <84149325+Sambitcr-7@users.noreply.github.com>
2022-03-08 06:08:06 +0530

    Add files via upload

---
+++ Escape.py
@@ -0,0 +1,14 @@
+#please dont remove this line 
+# """"
+# This is a 
+# Multiline Comment
+"""
+This is a comment 
+"""
+# print ("Codewithsam now", "Bhai video bhi like ker dena")
+# print("next line")
+# print("C:\sam")
+
+print("Harry is \n good boy \t1")
+
+
