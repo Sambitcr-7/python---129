4f00b7eda8c768f3a313dbb50dc4af06ae477f44
SAMBIT PATRA <84149325+Sambitcr-7@users.noreply.github.com>
2022-03-15 06:53:46 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,9 @@
+mystr = "Sam is a good boy"
+# print(len(mystr))
+# print(mystr[0:2])
+# print(mystr[::-2])
+
+print(mystr.isalnum())
+print(mystr.count("o"))
+print(mystr.capitalize())
+print(mystr.replace("is", "are"))
