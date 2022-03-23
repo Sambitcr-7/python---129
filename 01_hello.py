87d92cc68f94ec60c7d90dd37694436b822d3d8f
SAMBIT PATRA <84149325+Sambitcr-7@users.noreply.github.com>
2022-03-24 05:20:24 +0530

    Add files via upload

---
+++ 01_hello.py
@@ -0,0 +1,27 @@
+'''
+Author:Harry
+ Licened to : ABC Company
+ ********Thanks for reading************
+ '''''
+import os # importing the os module
+
+print("Hello world")
+
+
+# // Code testing-----
+# from sys import stdout
+# lo = 1
+# hi = 100000
+# ans = -1
+# while(lo<=hi):
+#     mid = (lo+  hi)//2
+#     print(mid)
+#     stdout.flush()
+#     s = input()
+#     if(s == "<"):
+#         hi = mid - 1
+#     else:
+#         ans = max(ans , mid)
+#         lo = mid + 1
+# print("! ", ans) 
+# stdout.flush()           
---
+++ 03_pr_03__module_usage.py
@@ -0,0 +1,3 @@
+from playsound import playsound 
+playsound('D:\\MyData\\Business\\code playground\\Python
+Course With Notes\\1.Chapter 1\\play.mp3')
