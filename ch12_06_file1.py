a7be2f6915459009555f6997d5f3b9a34f6e0d54
SAMBIT PATRA <84149325+Sambitcr-7@users.noreply.github.com>
2022-03-31 07:12:21 +0530

    Add files via upload

---
+++ ch12_06_file1.py
@@ -0,0 +1,8 @@
+def greet(name):
+    print(f"Good moring , {name}")
+
+
+print(__name__)
+if __name__=="___main__":
+    n = input("Enter a name\n")
+    greet(n)
---
+++ ch12_14_04_pr.py
@@ -0,0 +1,7 @@
+a = int(input("Enter number a:"))
+b = int(input("Enter number B:"))
+
+try:
+    print(a/b)
+except:
+    print("Infinite")    
